fd28541a2bee1db8ecf86fc2b814830d2cb2fb9f
huang <2532997991@qq.com>
2020-10-04 22:45:28 +0800

    '详情页开发一半'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,23 +1,83 @@
 <template>
-<div class="">
-
-</div>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+    </scroll>
+  </div>
 </template>
 
 <script>
+import DetailNavBar from "@/views/detail/childComps/DetailNavBar";
+import DetailSwiper from "@/views/detail/childComps/DetailSwiper";
+import DetailBaseInfo from "@/views/detail/childComps/DetailBaseInfo";
+import DetailShopInfo from "@/views/detail/childComps/DetailShopInfo";
+import DetailGoodsInfo from "@/views/detail/childComps/DetailGoodsInfo";
+
+import Scroll from "@/components/common/scroll/Scroll";
+
+import {getDetail, Goods, Shop} from "@/network/detail";
+
 export default {
   name: "Detail",
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {}
     }
   },
   created() {
+    // 1.保存传入的iid
     this.iid = this.$route.params.iid
+
+    // 2.根据iid请求详情数据
+    getDetail(this.iid).then(res => {
+      // 获取顶部的图片轮播数据
+      const data = res.result;
+      this.topImages = data.itemInfo.topImages
+      // 获取商品信息
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+      // 创建店铺信息对象
+      this.shop = new Shop(data.shopInfo)
+      // 保存商品的详情数据
+      this.detailInfo = data.detailInfo
+    })
+  },
+  components: {
+    DetailNavBar,
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    Scroll
+  },
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh()
+    }
   }
 }
 </script>
 
 <style scoped>
-
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+.content {
+  height: calc(100% - 44px);
+}
 </style>
